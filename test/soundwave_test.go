bcc9f06a72e8c2e5acd652e8d4b25f4cbf56c864
Allan Nava <allannava95@gmail.com>
2023-05-02 11:58:08 +0200

    updated

--- test/soundwave_test.go
+++ test/soundwave_test.go
@@ -1,6 +1,33 @@
 package soundwave
 
+import (
+	"github.com/Paxx-RnD/go-ffmpeg/configuration"
+)
+
 
 var (
 	inputPath      = "input.mp3"
 )
+
+
+func Configuration() *configuration.Configuration {
+	return &configuration.Configuration{
+		FfprobePath: "ffprobe",
+	}
+}
+//
+func TestNewService(t *testing.T) {
+	flags := getFlags()
+	ffprobe := ffprobe.NewFfProbe(Configuration(), nil)
+	/*s := sprite.NewService(flags, ffprobe)
+	assert.NotNil(t, s)*/
+}
+
+
+func getFlags() types.Flags {
+	flags := types.Flags{
+		Input:     inputPath,
+		Prefix:    outputPath,
+	}
+	return flags
+}
